e93ba3a20c4bdf051ca12d5e1e75230e1c88dbbb
Jane Greene <janegreenecasa@gmail.com>
2020-08-21 17:07:28 -0600

    changed create to send inverses to model for create

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,15 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:friend_email])
-    friendship = Friendship.create!(user_id: current_user.id, friend_id: friend.id) if friend
+    # friendship = Friendship.create!(user_id: current_user.id, friend_id: friend.id) if friend
+    #this creates the inverse relationship, destroy will need similar treatment
+    if friend
+      friendships = [
+                      {user_id: current_user.id, friend_id: friend.id},
+                      {friend_id: current_user.id, user_id: friend.id }
+                    ]
+      friendship = Friendship.create!(friendships) #if friend
+    end
     if friendship
       flash[:success] = 'You have a new Friend.'
     else
